858fc270dcc87d15251deb2f9ceb8d84f2d75ffd
Александр <frodomoget@gmail.com>
2023-10-26 17:08:34 +0300

    правка за настей

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-
 import mqtt.contact_mqtt
 import Optimize.optimize
 import CreateJson.create_json_param_DGU
@@ -48,7 +47,8 @@ def main():
         optimize.optimize(freq.P_DES_new)
         publish.optimize_publish(optimize)
         publish.regulation_frequency(freq)
-        print(11)
+
+
 
 if __name__ == '__main__':
     main()
