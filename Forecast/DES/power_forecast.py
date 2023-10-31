da87ac4b15ee4e4608d998c4af3f0e62d98a3749
Александр <frodomoget@gmail.com>
2023-11-01 02:05:38 +0300

    ИСПРАВЛЕНЫ БОЛЬШИЕ ОШИБКИ, описание ост прежним

--- Forecast/DES/power_forecast.py
+++ Forecast/DES/power_forecast.py
@@ -7,12 +7,13 @@ interface = Interface.interface
 
 class PowerForecast(interface.InterfaceForecast):
 
-    def __init__(self):
+    def __init__(self, mqttc):
         self.flag_power_forecast = None
         self.power_forecast = 0
+        self.mqttc = mqttc
 
-    def callback_data(self, mqttc, topic="mpei/Forecast/Load"):
-        mqttc.message_callback_add(topic, self.get_data)
+    def callback_data(self, topic="mpei/Forecast/Load"):
+        self.mqttc.message_callback_add(topic, self.get_data)
 
     def get_data(self, client, userdata, data):
         self.power_forecast = json.loads(data.payload.decode("utf-8", "ignore"))
--- Forecast/SES/power_farecast.py
+++ Forecast/SES/power_farecast.py
@@ -6,12 +6,14 @@ interface = Interface.interface
 
 
 class PowerForecast(interface.InterfaceForecast):
-    def __init__(self):
+
+    def __init__(self, mqttc):
         self.flag_power_forecast = None
         self.power_forecast = 0
+        self.mqttc = mqttc
 
-    def callback_data(self, mqttc, topic="mpei/Forecast/SES/Power"):
-        mqttc.message_callback_add(topic, self.get_data)
+    def callback_data(self, topic="mpei/Forecast/SES/Power"):
+        self.mqttc.message_callback_add(topic, self.get_data)
 
     def get_data(self, client, userdata, data):
         self.power_forecast = json.loads(data.payload.decode("utf-8", "ignore"))
